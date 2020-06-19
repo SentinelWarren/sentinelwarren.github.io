0dadc997e2e5d7e937ea6380f88b282c68b406da
SentinelWarren <warrenkalolo@gmail.com>
2020-06-19 21:26:28 +0300

    Adding gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    siteMetadata: {
+        name: "SentinelWarren's Portfolio",
+        description: "A blog and portfolio site for Warren Kalolo",
+        keywords: ["tech", "blog", "computer scientist", "fullstack developer", "data scientist", "frontend dev", "sentinelwarren", "Warren Kalolo", "vue dev", "react dev", "python dev", "javascript dev", "typescript dev"],
+        siteUrl: "https://sentinelwarren.tech",
+        siteImage: "images/",
+        profileImage: `images/`,
+        lang: `en, sw`,
+        config: {
+            sidebarWidth: 280
+        }
+    },
+    plugins: [{
+            resolve: "@pauliescanlon/gatsby-theme-terminal",
+            options: {
+                source: ["posts", "projects"]
+            }
+        },
+        {
+           resolve: `gatsby-plugin-canonical-urls`,
+           options: {
+               siteUrl: `https://sentinelwarren.tech`,
+           },
+        }
+    ]
+};
